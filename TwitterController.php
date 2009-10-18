d0135246c8a369f839e2b91199c03f4de7ec3661
andrew-waters <andrew@band-x-media.co.uk>
2009-10-18 19:38:52 +0000

    Removed contact page
    git-svn-id: file://localhost/Users/Andrew/Desktop/cvs/svn-repos/twitter@2 ef2f91c0-f156-45b4-90d8-eaf17f2a1d56

--- TwitterController.php
+++ TwitterController.php
@@ -1,27 +1,18 @@
 <?php
 
 class TwitterController extends PluginController {
-
-	var $path;
-	var $fullpath;
 	
-	public function __construct()
-	{
+	public function __construct() {
 		$this->setLayout('backend');
-		$this->assignToLayout('sidebar', new View('../../../plugins/twitter/views/sidebar'));
+		$this->assignToLayout('sidebar', new View('../../plugins/twitter/views/sidebar'));
 	}
 	
-	public function index()
-	{
+	public function index() {
 		$this->display('twitter/views/index');
 	}
 
 	public function extending() {
 		$this->display('twitter/views/extending');
-	}
-
-	public function contact() {
-		$this->display('twitter/views/contact');
 	}
 
 }
