f40dca11926b941867e21ea6c3b8088605b5c58c
andrew-waters <andrew@band-x-media.co.uk>
2009-10-18 19:39:06 +0000

    Updated for Wolf
    git-svn-id: file://localhost/Users/Andrew/Desktop/cvs/svn-repos/twitter@3 ef2f91c0-f156-45b4-90d8-eaf17f2a1d56

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <?php
 
-
 Plugin::setInfos(array(
-    'id'          => 'twitter',
-    'title'       => 'Twitter', 
-    'description' => 'Adds your Twitter status to your site', 
-    'version'     => '0.1',
-    'author'      => 'Andrew Waters',
-    'website'     => 'mailto:andrew@band-x.org')
-);
+	'id'			=> 'twitter',
+	'title'			=> 'Twitter', 
+	'description'	=> 'Adds your Twitter status to your site', 
+	'version'		=> '0.1',
+	'author'		=> 'Andrew Waters',
+	'website'		=> 'http://www.band-x.org'
+));
 
-
-Plugin::addController('twitter', 'Twitter');
-
+Plugin::addController('twitter', 'Twitter', 'administrator,developer,editor', TRUE);
 
 // Include Functions
 include('functions/twitter_js.php');
