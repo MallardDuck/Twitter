92fdf6cf49bb196aab44a3a48df93485066357e7
andrew-waters <andrew@band-x-media.co.uk>
2009-10-18 20:30:36 +0000

    Fixed for release - updated version
    git-svn-id: file://localhost/Users/Andrew/Desktop/cvs/svn-repos/twitter@7 ef2f91c0-f156-45b4-90d8-eaf17f2a1d56

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Plugin::setInfos(array(
 	'id'			=> 'twitter',
 	'title'			=> 'Twitter', 
 	'description'	=> 'Adds your Twitter status to your site', 
-	'version'		=> '0.2',
+	'version'		=> '0.2.0',
 	'update_url'	=> 'http://www.band-x.org/update.xml',
 	'author'		=> 'Andrew Waters',
 	'website'		=> 'http://www.band-x.org'
